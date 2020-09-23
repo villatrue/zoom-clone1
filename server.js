d76f8372033bf9570fa75e20942886fe085dc1c6
villatrue <scarter0313@gmail.com>
2020-09-23 16:20:41 -0700

    video working, next step fix logout lag

--- server.js
+++ server.js
@@ -19,6 +19,10 @@ app.get('/:room', (req, res) => {
 io.on('connection', socket =>{ // socket that usere is connecting to
   socket.on('join-room', (roomId, userId) => { // calls join room and adds room and user
     socket.join(roomId)
+    socket.to(roomId).broadcast.emit('user-connected', userId) // broadcast sends the message to everyone in the room besides the new user
+    socket.on('disconnect', () =>{
+      socket.to(roomId).broadcast.emit('user-disconnected', userId)
+    })
   })
 })
 
