acee2e1477d905428aa298488ddf8d05bdc6af55
villatrue <scarter0313@gmail.com>
2020-10-08 14:06:15 -0700

    mute function working

--- public/script.js
+++ public/script.js
@@ -7,12 +7,15 @@ const myPeer = new Peer(undefined, {
 const myVideo = document.createElement("video");
 myVideo.muted = true;
 let peers = {};
+let myVideoStream
+
 navigator.mediaDevices
   .getUserMedia({
     video: true,
     audio: true
   })
   .then(stream => {
+    myVideoStream = stream
     addVideoStream(myVideo, stream);
 
     myPeer.on("call", call => {
@@ -71,6 +74,45 @@ function addVideoStream(video, stream) {
   });
   videoGrid.append(video);
 }
-const scrollToBottom = () = => {
-  var d = $('.main__chat_window')
+const scrollToBottom = () => {
+  let d = $('.main__chat_window');
+  d.scrollTop(d.prop("scrollHeight"));
+}
+
+const muteUnmute = () =>{
+  const enabled = myVideoStream.getAudioTracks()[0].enabled;
+  if (enabled) {
+    myVideoStream.getAudioTracks()[0].enabled = false;
+    setUnmuteButton();
+  } else {
+    setMuteButton()
+    myVideoStream.getAudioTracks()[0].enabled = true;
+  }
+}
+
+const setMuteButton = () =>{
+  const html = `
+    <i class= "fas fa-microphone"></i>
+    <span>Mute</>
+  `
+  document.querySelector('.main__mute_button').innerHTML = html;
+}
+
+const setUnmuteButton = () =>{
+  const html = `
+    <i class= "unmute fas fa-microphone-slash"></i>
+    <span>Unmute</>
+  `
+  document.querySelector('.main__mute_button').innerHTML = html;
+}
+
+const playStop = () => {
+  let enabled = myVideoStream.getVideoTracks()[0].enabled;
+  if (enabled){
+    myVideoStream.getVideoTracks()[0].enabled = false
+    setPlayVideo()
+  } else{
+    setStopVideo()
+    myVideoStream.getVideoTracks()[0].enabled = true
+  }
 }
